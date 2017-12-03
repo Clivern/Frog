bc0bb87ed0c7368ce3a0a066676bfed24445ea77
Clivern <hello@clivern.com>
2017-12-03 19:26:10 +0200

    facebook info class ident fixed

--- src/main/java/com/clivern/frog/provider/facebook/Info.java
+++ src/main/java/com/clivern/frog/provider/facebook/Info.java
@@ -23,22 +23,22 @@ import com.clivern.frog.exception.InvalidProviderConfigs;
  */
 public class Info {
 
-	public static final String oauthURL = "https://www.facebook.com/v2.11/dialog/oauth";
+    public static final String oauthURL = "https://www.facebook.com/v2.11/dialog/oauth";
 
-	/**
-	 * Get Oauth URL (Login URL)
-	 *
-	 * @param  client_id              The ID of your app, found in your app's dashboard.
-	 * @param  redirect_uri           The URL that you want to redirect the person logging in back to
-	 * @param  state                  An arbitrary unique string created by your app to guard against Cross-site Request Forgery.
-	 * @param  response_type          Determines whether the response data included when the redirect back to the app occurs is in URL parameters or fragments
-	 * @param  scope                  A comma separated list of Permissions to request from the person using your app.
-	 * @return String
-	 * @throws InvalidProviderConfigs
-	 */
-	public static final String getOauthURL(String client_id, String redirect_uri, String state, String response_type, String scope) throws InvalidProviderConfigs
-	{
-		String url = Info.oauthURL;
+    /**
+     * Get Oauth URL (Login URL)
+     *
+     * @param  client_id              The ID of your app, found in your app's dashboard.
+     * @param  redirect_uri           The URL that you want to redirect the person logging in back to
+     * @param  state                  An arbitrary unique string created by your app to guard against Cross-site Request Forgery.
+     * @param  response_type          Determines whether the response data included when the redirect back to the app occurs is in URL parameters or fragments
+     * @param  scope                  A comma separated list of Permissions to request from the person using your app.
+     * @return String
+     * @throws InvalidProviderConfigs
+     */
+    public static final String getOauthURL(String client_id, String redirect_uri, String state, String response_type, String scope) throws InvalidProviderConfigs
+    {
+        String url = Info.oauthURL;
 
         if( client_id.isEmpty() || redirect_uri.isEmpty() || state.isEmpty() ){
             throw new InvalidProviderConfigs("Facebook Oauth URL is Invalid! This may be due to missing Client ID, Redirect URI or State.");
@@ -64,23 +64,23 @@ public class Info {
             url += "&scope=" + scope;
         }
 
-		return url;
-	}
+        return url;
+    }
 
-	/**
-	 * Get Declined Oauth URL (Re-asking for Declined Permissions)
-	 *
-	 * @param  client_id              The ID of your app, found in your app's dashboard.
-	 * @param  redirect_uri           The URL that you want to redirect the person logging in back to
-	 * @param  state                  An arbitrary unique string created by your app to guard against Cross-site Request Forgery.
-	 * @param  response_type          Determines whether the response data included when the redirect back to the app occurs is in URL parameters or fragments
-	 * @param  scope                  A comma separated list of Permissions to request from the person using your app.
-	 * @return String
-	 * @throws InvalidProviderConfigs
-	 */
-	public static final String getDeclinedOauthURL(String client_id, String redirect_uri, String state, String response_type, String scope) throws InvalidProviderConfigs
-	{
-		String url = Info.oauthURL;
+    /**
+     * Get Declined Oauth URL (Re-asking for Declined Permissions)
+     *
+     * @param  client_id              The ID of your app, found in your app's dashboard.
+     * @param  redirect_uri           The URL that you want to redirect the person logging in back to
+     * @param  state                  An arbitrary unique string created by your app to guard against Cross-site Request Forgery.
+     * @param  response_type          Determines whether the response data included when the redirect back to the app occurs is in URL parameters or fragments
+     * @param  scope                  A comma separated list of Permissions to request from the person using your app.
+     * @return String
+     * @throws InvalidProviderConfigs
+     */
+    public static final String getDeclinedOauthURL(String client_id, String redirect_uri, String state, String response_type, String scope) throws InvalidProviderConfigs
+    {
+        String url = Info.oauthURL;
 
         if( client_id.isEmpty() || redirect_uri.isEmpty() || state.isEmpty() ){
             throw new InvalidProviderConfigs("Facebook Oauth URL is Invalid! This may be due to missing Client ID, Redirect URI or State.");
@@ -108,6 +108,6 @@ public class Info {
             url += "&scope=" + scope;
         }
 
-		return url;
-	}
+        return url;
+    }
 }
